7c7aefcded6524f7ca7943f66baf8ca4a658d9b2
Suraj Vishwanath <surajv629@gmail.com>
2015-07-22 09:26:38 +0530

    added facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root "pages#home"
 
   get "about" => "pages#about"
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
 
   get "contact" => "pages#contact"
 
-  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
